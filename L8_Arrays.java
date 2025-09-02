38fb9acd0f49c117b95f9a56a6d24ae1e613f639
Lavish Sisodia <lavishsisodia9870@gmail.com>
2025-09-02 23:45:35 +0530

    L8_Array

---
+++ L8_Arrays.java
@@ -0,0 +1,94 @@
+import java.util.Scanner;
+
+
+//Marks
+/*public class L8_Arrays {
+    
+    public static void main(String[] args) {
+        
+        int[] marks = new int[3];
+        marks[0] = 95; //phy
+        marks[1] = 97; //chem
+        marks[2] = 99; //math
+
+        //print chemistry marks
+        System.out.println(marks[1]);
+
+    }
+}*/
+
+
+
+
+
+// Marks by for loop in array
+// public class L8_Arrays {
+    
+//     public static void main(String[] args) {
+        
+//         int[] marks = new int[3];
+//         marks[0] = 95; //phy
+//         marks[1] = 97; //chem
+//         marks[2] = 99; //math
+
+//         for (int i = 0; i <= 3; i++) {
+//             System.out.println(marks[i]);
+//         }
+
+//     }
+// }
+
+
+
+
+
+
+// public class L8_Arrays {
+    
+//     public static void main(String[] args) {
+
+//         Scanner input = new Scanner(System.in);
+//         int size = input.nextInt();
+//         int[] numbers = new int[size];
+
+//         // input
+//         for (int i = 0; i < size; i++) {
+//             numbers[i] = input.nextInt();
+//         }
+
+
+//         for (int i = 0; i < size; i++){
+//             System.out.println(numbers[i]);
+//        }
+
+//     }
+// }
+
+
+
+
+
+public class L8_Arrays {
+    
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int size = input.nextInt();
+        int[] num = new int[size];
+
+        // input
+        for (int i = 0; i < size; i++) {
+            num[i] = input.nextInt();
+        }
+
+        int x = input.nextInt();
+
+
+        for (int i = 0; i < size; i++){
+            if (num[i] == x) {
+                System.out.println("x is found at index : "+i);
+            }
+       }
+
+    }
+}
